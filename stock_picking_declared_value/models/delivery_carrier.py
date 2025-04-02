OCA/delivery-carrier

add stock_picking_declared_value

stock_picking_declared_value/models/delivery_carrier.py
@@ -1,0 +1,15 @@
+# Copyright 2025 Nitrokey GmbH
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class DeliveryCarrier(models.Model):
+    _inherit = "delivery.carrier"
+
+    declared_amount = fields.Float(
+        string="Declared Amount (%)",
+        default=100.0,
+        help="Percentage of the sale price to be used as the declared value for shipping. "
+        "100% means the full sale price will be used.",
+    )

Nitrokey/odoo-delivery-carrier

stock_picking_declared_value/models/delivery_carrier.py
@@ -1,0 +1,15 @@
+# Copyright 2025 Nitrokey GmbH
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class DeliveryCarrier(models.Model):
+    _inherit = "delivery.carrier"
+
+    declared_amount_percentage = fields.Float(
+        string="Declared Amount (%)",
+        default=100.0,
+        help="Percentage of the sale price to be used as the declared value for shipping. "
+        "100% means the full sale price will be used.",
+    )