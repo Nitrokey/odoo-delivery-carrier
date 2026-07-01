e51fdb3ded55d5c764108d9e691476d6f96600eb
Jan Suhr <jan@nitrokey.com>
2026-07-01 13:15:01 +0200

    use base_customs_territory for paperless invoice

--- delivery_ups_oca/__manifest__.py
+++ delivery_ups_oca/__manifest__.py
@@ -16,6 +16,7 @@
         "delivery_package_number",
         "delivery_price_method",
         "delivery_state",
+        "base_customs_territory",
     ],
     "data": [
         "data/stock_package_type_data.xml",
